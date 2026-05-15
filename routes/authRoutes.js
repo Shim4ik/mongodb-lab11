c5ab280388f7f7e81031c69111837c94ce75ad41
Shim4ik <kytsomelya@gmail.com>
2026-05-15 17:44:25 +0300

    update routes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,7 +2,11 @@ const express = require('express');
 const router = express.Router();
 const { register, login, getMe } = require('../controllers/authController');
 const protect = require('../middlewares/protect');
-router.post('/register', register);
-router.post('/login', login);
-router.get('/me', protect, getMe); // захищений маршрут
+const validate = require('../middlewares/validate');
+const { registerSchema, loginSchema } = require('../validators/authValidator');
+
+router.post('/register', validate(registerSchema), register);
+router.post('/login', validate(loginSchema), login);
+router.get('/me', protect, getMe);
+
 module.exports = router;
